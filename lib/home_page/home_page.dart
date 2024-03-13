cd007e0f70a76bd40e3839c137fc8f3e77c0c109
Yuri Bueno <yuri.bueno@vrsoft.com.br>
2024-03-13 08:27:50 -0300

    feat: Estrutura pdv

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -13,34 +13,68 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Container(
-          width: MediaQuery.of(context).size.height * 1,
-          height: MediaQuery.of(context).size.width * 0.6,
-          decoration: BoxDecoration(
-              border: Border.all(
-            width: 5,
-            color: Colors.blue,
-          )),
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 1,
-                height: MediaQuery.of(context).size.height * 0.1,
-                decoration: BoxDecoration(
-                    border: Border.all(width: 5, color: Colors.red)),
+        child: Row(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.height * 1,
+              height: MediaQuery.of(context).size.width * 0.6,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                width: 5,
+                color: Colors.blue,
+              )),
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 1,
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 5, color: Colors.red)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 1,
+                      height: MediaQuery.of(context).size.height * 0.1,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 5, color: Colors.red)),
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 1,
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 5, color: Colors.red)),
+            ),
+            const Spacer(),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.36,
+              height: MediaQuery.of(context).size.height * 1,
+              decoration: BoxDecoration(
+                  border: Border.all(width: 5, color: Colors.blue)),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.36,
+                      height: MediaQuery.of(context).size.height * 0.65,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 5, color: Colors.red)),
+                    ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5.0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.36,
+                        height: MediaQuery.of(context).size.height * 0.2,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 5, color: Colors.purple)),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
