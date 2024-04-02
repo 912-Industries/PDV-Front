b82f0b075fa7749a8e5fce744db7de7dc13149a2
ybueno16 <yuribueno27@gmail.com>
2024-04-02 20:21:36 -0300

    feat: adicionar descrio produto bipado

---
+++ lib/screens/home_page/widgets/listagem_preco_produto/card_descritivo_produto.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class CardDescritivoProduto extends StatefulWidget {
+  final double preco;
+  final int quantidade;
+  final double total;
+
+  const CardDescritivoProduto(
+      {Key? key,
+      required this.preco,
+      required this.quantidade,
+      required this.total})
+      : super(key: key);
+
+  @override
+  State<CardDescritivoProduto> createState() => _CardDescritivoProdutoState();
+}
+
+class _CardDescritivoProdutoState extends State<CardDescritivoProduto> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 50),
+      width: MediaQuery.of(context).size.width * 1,
+      height: 70,
+      decoration: BoxDecoration(
+          border: Border.all(width: 2, color: Colors.black),
+          borderRadius: BorderRadius.circular(15)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: [
+                  const Text(
+                    'Preço',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  Text(widget.preco.toString()),
+                ],
+              ),
+              const Text('X'),
+              Column(
+                children: [
+                  const Text(
+                    'Quantidade',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  Text(widget.quantidade.toString()),
+                ],
+              ),
+              const Text('='),
+              Column(
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  Text(widget.total.toString()),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_page/widgets/listagem_preco_produto/listagem_produto.dart
+++ lib/screens/home_page/widgets/listagem_preco_produto/listagem_produto.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pdv_front/screens/home_page/widgets/listagem_preco_produto/card_descritivo_produto.dart';
 import 'package:pdv_front/screens/home_page/widgets/listagem_preco_produto/utils/card.dart';
 
 class ListagemProduto extends StatefulWidget {
@@ -21,18 +22,17 @@ class _ListagemProdutoState extends State<ListagemProduto> {
           ),
           borderRadius: BorderRadius.circular(20)),
       padding: const EdgeInsets.all(20),
-      child: Column(
+      child: const Column(
         children: [
-          const CardListagemProduto(
+          CardListagemProduto(
             descricaoProduto: 'teste',
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 15),
-            child: Container(
-              width: MediaQuery.of(context).size.width * 1,
-              height: MediaQuery.of(context).size.height * 0.1,
-              decoration: BoxDecoration(
-                  border: Border.all(width: 5, color: Colors.red)),
+            padding: EdgeInsets.only(top: 15),
+            child: CardDescritivoProduto(
+              preco: 1.2,
+              quantidade: 1,
+              total: 1.5,
             ),
           ),
         ],
