cee93e3413ff38705e780cb0e87b5cdf4003cef8
ybueno16 <yuribueno27@gmail.com>
2024-04-05 17:42:06 -0300

    feat alteração widgets

--- lib/screens/home_page/home_page.dart
+++ lib/screens/home_page/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pdv_front/screens/home_page/widgets/keyboard_buffer.dart';
+import 'package:pdv_front/screens/home_page/widgets/listagem_descritivo_produto/listagem_produto.dart';
 import 'package:pdv_front/screens/home_page/widgets/listagem_preco_produto/listagem_preco_produto.dart';
-import 'package:pdv_front/screens/home_page/widgets/listagem_preco_produto/listagem_produto.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
--- lib/screens/home_page/widgets/listagem_preco_produto/card_listagem_preco_produto.dart
+++ lib/screens/home_page/widgets/listagem_preco_produto/card_listagem_preco_produto.dart
@@ -6,14 +6,18 @@ class CardListagemPreco extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-     padding: const EdgeInsets.all(20), 
+      padding: const EdgeInsets.all(20),
       width: MediaQuery.of(context).size.width * 1,
       height: 75,
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.black, width: 2),
-        borderRadius: BorderRadius.circular(5)
-      ),
+          border: Border.all(color: Colors.black, width: 2),
+          borderRadius: BorderRadius.circular(5)) ,
+          child:  const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(children: [Text('teste'),],),
+          ],
+          ),
     );
-
   }
 }
